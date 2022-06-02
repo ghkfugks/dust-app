1b5701b02c3b47990c4e42c68e53f47e9c9811d8
jung hyun <hfghhghg11@gmil.com>
2022-06-02 10:15:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,19 @@ from app_data import run_data
 from app_home import run_home
 from app_ml import run_ml
 
+from streamlit_option_menu import option_menu
+with st.sidebar:
+    
+        choose = option_menu("Menu", menu,
+                            icons=['house', 'graph-up'],
+                            menu_icon="app-indicator", default_index=0,
+                            styles={
+            "container": {"padding": "5!important", "background-color": "#44475A5"},
+            "icon": {"color": "orange", "font-size": "25px"}, 
+            "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#BD93F9"},
+            "nav-link-selected": {"background-color": "#02ab21"},
+        }
+
 
 
 def main():
