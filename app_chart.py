198bdb6fd7be0de7f334f966f009ab6fce310486
jung hyun <hfghhghg11@gmil.com>
2022-06-08 10:09:01 +0900

    Update app_chart.py

--- app_chart.py
+++ app_chart.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from PIL import Image
+import plotly.express as px
 
 ############### 그래프에서 한국어 인식 ###############
 import platform
@@ -26,6 +27,22 @@ def run_chart():
     df = pd.read_csv('data/fine_dust.csv',encoding ='cp949' )
 
     
+    sns.set_theme(style="whitegrid")
+
+    fig = plotly.bar(df, x='풍속', y='연월일')
+    st.plotly_chart(fig)
+
+    fig4 = plt.figure()
+    df['유동인구'].hist()
+    st.pyplot(fig4)
+
+    fig2 = plt.figure()
+    df['미세먼지'].hist()
+    st.pyplot(fig2)
+
+    fig3 = plt.figure()
+    df['초미세먼지'].hist()
+    st.pyplot(fig3)
 
 
 
